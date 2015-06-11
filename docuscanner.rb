19251ff187e5d411362243b83f01d4508fa604b2
mark <droidorro@gmail.com>
2015-06-10 22:12:55 -0700

    z

--- docuscanner.rb
+++ docuscanner.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
-
+require 'pdf-reader'
 ## Scan local and remote file systems for docs/pdfs insert into mongo
 
 #
 hostndir = Hash.new
-hostndir['localhost'] = '~/'
+hostndir['localhost'] = 'C:/users/mark/documents/'
 
 def scanfs_docs(hdir)
 pdflocs = []
@@ -22,7 +22,30 @@ doculocs
 end
 end
 
+def pdfreader(pdf2read)
+	reader = PDF::Reader.new(pdf2read)
 
+	puts reader.pdf_version
+	puts reader.info
+	puts reader.metadata
+	puts reader.page_count
+
+	reader.pages.each do |page|
+		puts page.fonts
+		puts page.text
+		puts page.raw_content
+	end
+end
+
+pdfreader('C:\Users\Mark\Documents\sick.pdf')
+
+
+__END__
+doclocs=scanfs_docs (hostndir)
+p doclocs
+doclocs.each do |docloc|
+pdfread=pdfreader(docloc)
+end
 
 
 
