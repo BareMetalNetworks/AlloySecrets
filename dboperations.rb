9bb7641cc147f63416de23ac6f31a81e6afb32ca
seshin <sam@baremetalnetworks.com>
2015-06-10 20:42:37 -0700

    foo

--- dboperations.rb
+++ dboperations.rb
@@ -49,4 +49,21 @@ class Words < ActiveRecord::Base
 	belongs_to :page
 end
 
+__END__
+
+page = Page.create!(:page_num '1', :document_id '1')
+doc = Document.create!(:name 'Foo', )
+# Create a few records...
+show = Show.create!(name: 'Big Bang Theory')
+
+first_episode = show.episodes.create!(name: 'Pilot')
+second_episode = show.episodes.create!(name: 'The Big Bran Hypothesis')
+
+episode_names = show.episodes.pluck(:name)
+
+puts "#{show.name} has #{show.episodes.size} episodes named #{episode_names.join(', ')}."
+# => Big Bang Theory has 2 episodes named Pilot, The Big Bran Hypothesis.
+
+# Use `binding.pry` here to experiment with this setup.
+=end
 
