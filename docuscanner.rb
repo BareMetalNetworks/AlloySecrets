7081fe5fb2b681bcd0aad749d78a5b3a47e74aca
seshin <sam@baremetalnetworks.com>
2015-06-10 20:41:09 -0700

    foo

--- docuscanner.rb
+++ docuscanner.rb
@@ -24,63 +24,6 @@ end
 
 
 
-# Run this script with `$ ruby my_script.rb`
-require 'mysql2'
-require 'active_record'
-
-# Use `binding.pry` anywhere in this script for easy debugging
-#require 'pry'
-
-# Connect to an in-memory sqlite3 database
-ActiveRecord::Base.establish_connection(
-		adapter: 'mysql2',
-		database: 'alloysecrets',
-	  host: '10.0.0.32',
-    user: 'alloysecets',
-	  password: 'password'
-
-
-)
-
-# Define a minimal database schema
-ActiveRecord::Schema.define do
-	create_table :documents, force: true do |t|
-		t.integer :id
-		t.string :name
-		t.string :path
-
-	end
-
-	create_table :pages, force: true do |t|
-		t.integer :id
-		t.integer :pagenum
-		t.integer :document_id
-	end
-
-	create_table :words, force: true do |t|
-		t.integer :id
-		t.string :wordly
-		t.integer :page_id
-
-	end
-
-	add_index "wordlists", ["page_id"], :name=> "doc_pages_words"
-
-end
-
-# Define the models
-class Document < ActiveRecord::Base
-	has_many :pages
-end
-
-class Page < ActiveRecord::Base
-	belongs_to :document # inverse_of: :foo, required: true
-end
-
-class Words < ActiveRecord::Base
-	belongs_to :words
-end
-=begin
 
 page = Page.create!(:page_num '1', :document_id '1')
 doc = Document.create!(:name 'Foo', )
