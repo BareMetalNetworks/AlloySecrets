e83de32bdf576e35f408f2438fef73cf7d29a33d
seshin <sam@baremetalnetworks.com>
2015-06-26 17:57:09 +0100

    book crud

---
+++ webui/app/models/book.rb
@@ -0,0 +1,11 @@
+class Book
+  include Mongoid::Document
+  field :title, type: String
+  field :author, type: String
+  field :info, type: String
+  field :version, type: String
+  field :metadata, type: String
+  field :count, type: Integer
+  field :timestamp, type: Time
+  field :page, type: String
+end
---
+++ webui/app/views/books/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @book, :id, :title, :author, :info, :version, :metadata, :count, :timestamp, :page, :created_at, :updated_at
